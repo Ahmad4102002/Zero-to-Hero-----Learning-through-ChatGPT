0bc05ccaac9c51a5b0b773b0d020b7cd98ec11a7
Ahmad Shah <150964575+Ahmad4102002@users.noreply.github.com>
2026-01-02 18:28:53 +0530

    Update url_shortner.py
    finally completed but need to work on global variables and where to place them. GPT says to avoid them.

--- Day-4-Url Shortner/url_shortner.py
+++ Day-4-Url Shortner/url_shortner.py
@@ -22,7 +22,7 @@ def uni_ip_url():
         if every[2] not  in uni:
             uni[every[2]] = {}
 
-        uni[every[2]][every[1]] = uni.get(every[1],0 ) +1
+        uni[every[2]][every[1]] = uni[every[2]].get(every[1],0 ) +1
     
     print(uni)
 
@@ -35,6 +35,24 @@ def failed_re():
         fail_pass[each[3]] = fail_pass.get(each[3], 0) + 1
     
     print(fail_pass["FAIL"])
+
+def status_codes_url():
+
+    global status_codes
+    status_codes = {}
+
+    
+    for every in liss:
+        
+        if every[1] not in status_codes:
+            status_codes[every[1]] = {}
+
+        status_codes[every[1]][every[3]] = status_codes[every[1]].get(every[3], 0) +1
+
+    print(status_codes)
+    
+    
+
 
 def reuest_count_ip():
 
@@ -52,7 +70,37 @@ def max_reuests_count():
 
 
 def final_report_url():
+   
+    print("Enter URL name : ")
+    b = input().strip()
+    
     url_count()
+    uni_ip_url()
+    failed_re()
+    status_codes_url()
+
+    print("-------------------------------------------------")
+    
+    print(f"{b} has be accessed {url_counts[b]} times ")
+
+    count = 0
+    for value in uni.values():
+        
+        if b in value:
+            count += 1
+        
+    print(f"{b} has been accesed {count} uniue times")
+
+    
+    for key,valu in uni.items():
+        
+        if b in valu:
+            print(f"{key} ip accessed {b} url  {valu[b]} times")
+    
+
+    if b in status_codes:
+        print(b , status_codes[b])
+
 
 
 def main():
@@ -61,6 +109,7 @@ def main():
     print("uniue ip and count of url accessed - 3")
     print("How many failed - 4")
     print("Max reuest per ip  - 5")
+    print("Print Report for URL - 6")
 
     n = int(input().strip())
 
@@ -78,6 +127,9 @@ def main():
 
     if n == 5:
         max_reuests_count()
+
+    if n == 6:
+        final_report_url()
 
 
 
